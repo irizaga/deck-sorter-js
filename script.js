cf074d4464f191270786e13df57c323cb8f882cf
irizaga <gibran.irizaga@gmail.com>
2022-03-04 17:43:16 -0300

    "Changed JS after first review"

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
-const suits = ['hearts', 'spades', 'diamonds', 'clubs'];
+const CARDS = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'];
+const SUITS = ['hearts', 'spades', 'diamonds', 'clubs'];
 
 const heartsButton = document.getElementById('button-hearts');
 const spadesButton = document.getElementById('button-spades');
@@ -20,13 +20,6 @@ resetButton.addEventListener('click', () => {
   populateDeck();
 });
 
-function showSuit(suit) {
-  const toshow = document.querySelectorAll('.' + suit);
-  setVisibility (toshow, 'visible');
-  const elements = document.querySelectorAll('#cards > div:not(.' + suit + ')'); 
-  setVisibility(elements, 'hidden');
-}
-
 heartsButton.addEventListener('click', () => { showSuit('hearts') })
 spadesButton.addEventListener('click', () => { showSuit('spades') })
 diamondsButton.addEventListener('click', () => { showSuit('diamonds') })
@@ -37,20 +30,29 @@ showallButton.addEventListener('click', () => {
   setVisibility(elements, 'visible');
 });
 
-function populateDeck () {
-  for (let count = 0; count < 4; count++) {
-    const cardsNodeList = cards.map( x => createNode(x, suits[count]));
+function showSuit(suit) {
+  const toshow = document.querySelectorAll(`.${suit}`);
+  setVisibility (toshow, 'visible');
+  const tohide = document.querySelectorAll(`#cards > div:not(.${suit})`); 
+  setVisibility(tohide, 'hidden');
+}
+
+function setVisibility(elements, visibility) {
+  elements.forEach(element => element.style.visibility = visibility);
+}
+
+function populateDeck() {
+  SUITS.forEach( suit => {
+    const cardsNodeList = CARDS.map( card => createNode(card, suit) );
     const container = document.querySelector("#cards");
     container.append(...cardsNodeList);
-  }
+  });
 }
 
 function deleteChildren() {
-  const e = document.querySelector("#cards");
-  let first = e.firstElementChild;
-  while (first) {
-      first.remove();
-      first = e.firstElementChild;
+  const wrapper = document.querySelector("#cards");
+  while (wrapper.firstElementChild) {
+      wrapper.firstElementChild.remove();
   }
 }
 
@@ -60,20 +62,15 @@ function createNode(card, suit) {
   cardDiv.classList.add('singlecard');
   const content = document.createTextNode(card);
   cardDiv.appendChild(content);
-
   return cardDiv;
-}
-
-function setVisibility(elements, visibility) {
-  elements.forEach(element => element.style.visibility = visibility);
 }
 
 function shuffle() {               
   const parent = document.querySelector('#cards');
   const divs = parent.children;
-  const frag = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
   while (divs.length) {
-    frag.appendChild(divs[Math.floor(Math.random() * divs.length)]);
+    fragment.appendChild(divs[Math.floor(Math.random() * divs.length)]);
   }
-  parent.appendChild(frag);
+  parent.appendChild(fragment);
 }
